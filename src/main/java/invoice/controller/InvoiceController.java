d3773a4252e1c0c8da2d3e9998472bb25001565d
Themba Makinita <jeffmakinita@gmail.com>
2018-07-26 11:52:00 +0200

    second commit

--- src/main/java/invoice/controller/InvoiceController.java
+++ src/main/java/invoice/controller/InvoiceController.java
@@ -2,37 +2,45 @@ package invoice.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import invoice.entity.Invoice;
+import invoice.repo.InvoiceRepo;
 
 @RestController
 public class InvoiceController {
 	
+	@Autowired
+	private InvoiceRepo invoiceData;
 	
 	@RequestMapping(value ="/invoices",method = RequestMethod.POST)
-	public String addInvoice(Invoice invoice) {
-		Invoice vInvoice = new Invoice();
-		vInvoice = invoice;
+	public void addInvoice(Invoice invoice) {
 		
-		return ("redirect:/invoices");
-		
+		invoiceData.save(invoice);
 	}
 	
 	@RequestMapping("/invoices")
 	public List<Invoice> viewAllInvoices(){
-		return null;
+		
+		
+		List<Invoice> allInvoice = invoiceData.findAll();
+		return allInvoice;
 		
 		
 	}
 	
-	@RequestMapping("/invoices/id")
-	public Invoice viewInvoice() {
+	@RequestMapping("/invoices/{id}")
+	public Invoice viewInvoice(@PathVariable String id) {
+		long iD = Long.parseLong(id);
+		if(invoiceData.existsById(iD)) {
+			return invoiceData.getOne(iD);
+		}
 		return null;
-		
-		
 	}
 	
 
